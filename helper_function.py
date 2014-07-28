0a4303f59add333dbae60f701b62b72c298a1af8
Sraddhanjali Acharya <tilaprimera@gmail.com>
2014-07-28 16:53:19 +0545

    creates folder in destination where folder name is same as source folder's name and saves up the result of processing of source file in that destination folder

--- helper_function.py
+++ helper_function.py
@@ -1,11 +1,16 @@
 import cv2
+import os
 import numpy as np
-import matplotlib
-from math import sqrt, log, exp
 
 
-def img_write(img):
-    cv2.imwrite("result.jpg", img)
+def img_write(img, full_filename, full_directory):
+    filename = os.path.basename(full_filename)
+    directory = os.path.basename(os.path.dirname(full_filename))
+    full_directory += directory
+    if not os.path.exists(directory):
+        os.makedirs(full_directory)
+    full_path = full_directory +"/" + filename
+    cv2.imwrite(full_path, img)
 
 
 def show_wait(img, delay):
@@ -21,17 +26,12 @@ def img_divide(img, size):
         height = column+size
         for row in range(0, img.shape[1], size):
             width = row+size
-            img[column:height, row:width] = img_adaptive_otsu(img[column:height, row:width])
+            temp = img[column:height, row:width]
+            temp = img_adaptive_otsu(temp)
+            img[column:height, row:width] = img_contour_smooth(temp)
     return img
 
-
-# def nd_array_value_replace(block, value):
-#     for ind1, value1 in enumerate(block):
-#         for ind2, value2 in enumerate(value1):
-#             block[ind1][ind2] = value
-#     return block
-
-
+#TODO learning based binarization aka SVM integration
 def img_adaptive_otsu(block):
     # print type(block)
     """This is where the adaptive threshold implementation occurs based on local otsu threshold value for each block """
@@ -41,12 +41,15 @@ def img_adaptive_otsu(block):
     mean_block = np.mean(block)
     sd_block = np.std(block)
     if sd_block > set_sd:
-        ret, block = cv2.threshold(block, 0, 255, cv2.THRESH_OTSU)
+        ret, block = cv2.threshold(block, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
     elif sd_block < set_sd:
         if mean_block > set_mean:
-            block[:] = 255
-        else:
             block[:] = 0
+        else:
+            block[:] = 255
     return block
 
-
+#TODO smooth and sharpen the binarized content to improve quality
+def img_contour_smooth(img):
+    img = cv2.GaussianBlur(img, (3, 3), 0.1)
+    return img
